66c1571427b27ee84f828f166c3f9e405ef6110b
Stewart ilondanga <stilondanga@gmail.com>
2018-09-09 21:45:19 +0300

    add index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-var navigate = (function() {
+/*var navigate = (function() {
 	$('.dd').toggle();
 	$('.dd_btn').click(function() {
 		var dataName = $(this).attr('data-name');
@@ -6,3 +6,22 @@ var navigate = (function() {
 		$('.' + dataName).toggle();
 	});
 })();
+
+for (const img of document.querySelectorAll("img.fx")) {
+  const { display } = getComputedStyle(img);
+  const { width, height } = img.getBoundingClientRect();
+
+  const wrapper = document.createElement("span");
+
+  // replace img by wrapper
+  wrapper.setAttribute("style", `display:${display};width:${width};height:${height};`);
+  wrapper.classList.add("fxwrap");
+  img.parentNode.replaceChild(wrapper, img);
+
+  // add original <img> to wrapper
+  wrapper.appendChild(img);
+
+  // add overlay layer to wrapper
+  const overlay = `<img src=${img.src} class="fxoverlay">`;
+  wrapper.insertAdjacentHTML("beforeend", overlay);
+}
