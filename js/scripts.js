55cb26130fa2f4791b271d02b4a1c801b026f32a
Stewart ilondanga <stilondanga@gmail.com>
2018-09-10 10:06:54 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -7,21 +7,26 @@ var navigate = (function() {
   });
 })();
 
-/*for (const img of document.querySelectorAll("img.fx")) {
-  const { display } = getComputedStyle(img);
-  const { width, height } = img.getBoundingClientRect();
+for (const img of document.querySelectorAll("img.fx")) {
+  const {
+    display
+  } = getComputedStyle(img);
+  const {
+    width,
+    height
+  } = img.getBoundingClientRect();
 
-  const wrapper = document.createElement("span");
+  /*  const wrapper = document.createElement("span");
 
-  // replace img by wrapper
-  wrapper.setAttribute("style", `display:${display};width:${width};height:${height};`);
-  wrapper.classList.add("fxwrap");
-  img.parentNode.replaceChild(wrapper, img);
+    // replace img by wrapper
+    wrapper.setAttribute("style", `display:${display};width:${width};height:${height};`);
+    wrapper.classList.add("fxwrap");
+    img.parentNode.replaceChild(wrapper, img);
 
-  // add original <img> to wrapper
-  wrapper.appendChild(img);
+    // add original <img> to wrapper
+    wrapper.appendChild(img);
 
-  // add overlay layer to wrapper
-  const overlay = `<img src=${img.src} class="fxoverlay">`;
-  wrapper.insertAdjacentHTML("beforeend", overlay);
-}
+    // add overlay layer to wrapper
+    const overlay = `<img src=${img.src} class="fxoverlay">`;
+    wrapper.insertAdjacentHTML("beforeend", overlay);
+  }
